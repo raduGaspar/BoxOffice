9d0a4aa0c06b43d1e10527eda2be3e62b5ad7bce
Radu Gaspar <radu.bogdan.gaspar@gmail.com>
2017-10-05 22:04:03 +0300

    added color constants

---
+++ app/constants/index.js
@@ -0,0 +1,13 @@
+export default {
+  colors: {
+    gray: {
+      light: '#f7f8fc',
+      medium: '#ecf0f1',
+      dark: '#34495e',
+      darker: '#2d3e50'
+    },
+    blue: '#2d70cc',
+    red: '#e74c3a',
+    green: '#2dcc70',
+  }
+}
