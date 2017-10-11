26a27245d6568fd7b015de7d384329f06436cf80
Radu Gaspar <radu.bogdan.gaspar@gmail.com>
2017-10-11 21:20:00 +0300

    code lint

--- app/containers/SettingsContainer.js
+++ app/containers/SettingsContainer.js
@@ -1,12 +1,12 @@
 export default () => (
   <div>
-    <label htmlFor="showTags">
-      <input id="showTags" type='checkbox' />
+    <label htmlFor='showTags'>
+      <input id='showTags' type='checkbox' />
       Display TV Show Tags
     </label>
-    <select name="" id="">
-      <option value="">English</option>
-      <option value="">Romanian</option>
+    <select name='' id=''>
+      <option value=''>English</option>
+      <option value=''>Romanian</option>
     </select>
   </div>
 )
