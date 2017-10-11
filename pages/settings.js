add8b1cac0e72bf724932cf2c5cda1684effa7fa
Radu Gaspar <radu.bogdan.gaspar@gmail.com>
2017-10-11 21:19:15 +0300

    partial i18n files

--- pages/settings.js
+++ pages/settings.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Layout from '../app/common/Layout'
+import SettingsContainer from '../app/containers/SettingsContainer'
 
 export default () => (
   <Layout>
-    <div>your settings page</div>
+    <SettingsContainer />
   </Layout>
 )
